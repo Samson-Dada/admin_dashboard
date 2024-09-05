a39fc85bf0a3e604c1bb93ebb15d848cc9801375
Samson Dada <57550434+Samson-Dada@users.noreply.github.com>
2024-09-05 17:52:47 +0100

    fix config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	build: {
+		outDir: "build", // Adjust this to match your expected output directory
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					// Example of chunking logic:
+					if (id.includes("node_modules")) {
+						return "vendor"; // All node_modules go to a single chunk named 'vendor'
+					}
+				},
+			},
+		},
+		chunkSizeWarningLimit: 500, // Adjust the limit as needed
+	},
+});
